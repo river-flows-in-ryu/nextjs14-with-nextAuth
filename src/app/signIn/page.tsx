b1d7ea7e9099c2dce2de4713582ce725bcb81d51
river-flows-in-ryu <rgr65066384@gmail.com>
2024-02-07 17:47:09 +0900

    nextAuth

---
+++ src/app/signIn/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+import { useSession } from "next-auth/react";
+
+export default function Page() {
+  const { data } = useSession();
+  console.log(data);
+  return (
+    //
+    <div></div>
+  );
+}
